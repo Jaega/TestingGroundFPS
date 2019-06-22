cf0f1314820e6aa1522910f75d2d6450a7f180ec
Xinzhao Li <szxzlee@gmail.com>
2019-06-22 16:00:17 +0800

    Convert Character to C++

---
+++ Source/TestingGroundFPS/Character/Mannequin.cpp
@@ -0,0 +1,52 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Mannequin.h"
+#include "../Weapons/Gun.h"
+#include "Camera/CameraComponent.h"
+#include "Components/CapsuleComponent.h"
+#include "Components/ChildActorComponent.h"
+
+// Sets default values
+AMannequin::AMannequin()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	FPCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("FPCamera"));
+	FPCamera->SetupAttachment(GetCapsuleComponent());
+	FPCamera->RelativeLocation = FVector(-39.56f, 1.75f, 64.f);
+	FPCamera->bUsePawnControlRotation = true;
+
+	FPArms = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("FPArms"));
+	FPArms->SetOnlyOwnerSee(true);
+	FPArms->SetupAttachment(FPCamera);
+	FPArms->bCastDynamicShadow = false;
+	FPArms->CastShadow = false;
+	FPArms->RelativeRotation = FRotator(1.9f, -19.19f, 5.2f);
+	FPArms->RelativeLocation = FVector(-0.5f, -4.4f, -155.7f);
+	
+}
+
+void AMannequin::BeginPlay()
+{
+	// Call the base class  
+	Super::BeginPlay();
+
+	if(!GunBlueprint) 
+	{ 
+		UE_LOG(LogTemp, Warning, TEXT("Gun blueprint missing."));
+		return; 
+	}
+	Gun = GetWorld()->SpawnActor<AGun>(GunBlueprint);
+
+	//Attach gun mesh component to Skeleton, doing it here because the skeleton is not yet created in the constructor
+	Gun->AttachToComponent(FPArms, FAttachmentTransformRules(EAttachmentRule::SnapToTarget, true), TEXT("GripPoint"));
+	Gun->AnimInstance = FPArms->GetAnimInstance();
+}
+
+
+void AMannequin::Fire()
+{
+	Gun->OnFire();
+}
+
---
+++ Source/TestingGroundFPS/Character/Mannequin.h
@@ -0,0 +1,40 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "Mannequin.generated.h"
+
+UCLASS()
+class TESTINGGROUNDFPS_API AMannequin : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	AMannequin();
+
+protected:
+	virtual void BeginPlay();
+
+public:	
+	UPROPERTY(EditDefaultsOnly, Category = Setup)
+	TSubclassOf<class AGun> GunBlueprint;
+
+	UFUNCTION(BlueprintCallable, Category = Weapon)
+	void Fire();
+
+private:
+
+	/** First person camera */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+	class UCameraComponent* FPCamera;
+	/** Pawn mesh: 1st person view (arms; seen only by self) */
+	UPROPERTY(VisibleDefaultsOnly, Category=Mesh)
+	class USkeletalMeshComponent* FPArms;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+	class AGun* Gun;
+
+};
